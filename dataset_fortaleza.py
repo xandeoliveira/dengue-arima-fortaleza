39736806e27323fdcb5a479dd994d13c34eb8216
Alexandre Lima de Oliveira <oliveiraalexandrelima@hotmail.com>
2025-05-15 13:41:40 -0300

    Create a file for getting Fortaleza city data.

---
+++ dataset_fortaleza.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+url =  "https://info.dengue.mat.br/api/alertcity?&disease=dengue" +
+    "&geocode=2304400&disease=dengue&format=csv&ew_start=1" +
+    "&ew_end=53&ey_start=2014&ey_end=2024"
