75a6098c0007dbc4a5ceb7196123283b4e809010
Alexandre Lima de Oliveira <oliveiraalexandrelima@hotmail.com>
2025-07-18 07:41:19 -0300

    Add 2025 dataset

--- dataset/dataset_fortaleza.py
+++ dataset/dataset_fortaleza.py
@@ -2,13 +2,13 @@ import pandas as pd
 from os import getcwd
 
 # URL da API do INFODENGUE
-url = "https://info.dengue.mat.br/api/alertcity?&disease=dengue&geocode=2304400&disease=dengue&format=csv&ew_start=1&ew_end=53&ey_start=2014&ey_end=2024"
+url = "https://info.dengue.mat.br/api/alertcity?&disease=dengue&geocode=2304400&disease=dengue&format=csv&ew_start=1&ew_end=26&ey_start=2014&ey_end=2025"
 df_fortaleza = pd.read_csv(url, index_col="SE")
 
 # Removendo as variáveis desnecessárias
 df_filtrado = df_fortaleza[["data_iniSE","casos"]]
-df_filtrado = df_filtrado.rename(columns={"data_iniSE":"ds", "casos":"y"})
-df_filtrado["unique_id"] = "1"
+# df_filtrado = df_filtrado.rename(columns={"data_iniSE":"ds", "casos":"y"})
+# df_filtrado["unique_id"] = "1"
 
 # Salvando dataframe...
-df_filtrado.to_csv(getcwd() + "/fortaleza-2024.csv")
+df_filtrado.to_csv(getcwd() + "/fortaleza-26-2025.csv")
